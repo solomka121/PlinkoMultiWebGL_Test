b5c26f3370a7d462fb894d1dfdacdf5cdd93f039
Slava <slavasolomahin12@gmail.com>
2024-11-08 05:53:17 +0200

    Gzip

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "Solma-RNG Build Piko-0.1";
 const contentToCache = [
     "Build/BuildRNGH 01.loader.js",
-    "Build/BuildRNGH 01.framework.js.br",
-    "Build/BuildRNGH 01.data.br",
-    "Build/BuildRNGH 01.wasm.br",
+    "Build/BuildRNGH 01.framework.js.gz",
+    "Build/BuildRNGH 01.data.gz",
+    "Build/BuildRNGH 01.wasm.gz",
     "TemplateData/style.css"
 
 ];
