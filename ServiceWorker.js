bfe057f8b3ccfc3f6b454661cdfcbb983d2fda5e
Slava <slavasolomahin12@gmail.com>
2024-11-08 05:42:20 +0200

    RNG Build Piko

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Solma-RNG Build Piko-0.1";
+const contentToCache = [
+    "Build/BuildRNGH 01.loader.js",
+    "Build/BuildRNGH 01.framework.js.br",
+    "Build/BuildRNGH 01.data.br",
+    "Build/BuildRNGH 01.wasm.br",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
